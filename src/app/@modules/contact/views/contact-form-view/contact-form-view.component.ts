ecd5314ac136ef584fb325fb97c7a394d85a93df
Leonardo Barrientos <leobarrientos@Leonardos-MacBook-Pro.local>
2020-02-11 22:16:39 -0600

    crear builder y algunos métodos

--- src/app/@modules/contact/views/contact-form-view/contact-form-view.component.ts
+++ src/app/@modules/contact/views/contact-form-view/contact-form-view.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+
+
 import { IContact } from '../../../../@core/contact/contact.model';
 
 
@@ -10,6 +14,17 @@ import { IContact } from '../../../../@core/contact/contact.model';
 })
 export class ContactFormViewComponent implements OnInit {
 
+	@Input() revert$: Observable<void>;
+	@Output() formChanges = new EventEmitter<any|false>();  // emits the form value
+	@Output() formValidChanges = new EventEmitter<boolean>();
+	@Output() formDirtyChanges = new EventEmitter<boolean>();
+
+  contact: IContact|null;
+  contactForm: FormGroup|null;
+
+
+  protected onDestroy$ = new Subject<void>();
+
   constructor(
     private formBuilder: FormBuilder
   ) { }
@@ -18,10 +33,41 @@ export class ContactFormViewComponent implements OnInit {
   }
 
 
+  setupForm() {
+		this.contactForm = this.buildForm(this.contact);
+		this.formChanges.emit(this.contactForm.value);
+		this.setupOutputs();
+	}
+
+	setupOutputs() {
+		this.contactForm.valueChanges.pipe(
+			takeUntil( this.onDestroy$ ),
+			tap( () => this.formValidChanges.emit( this.contactForm.valid ) ),
+			tap( () => this.formDirtyChanges.emit( this.contactForm.dirty ) )
+		).subscribe(() => {
+			if (this.contactForm.valid)
+				this.formChanges.emit(this.contactForm.value);
+			else
+				this.formChanges.emit(false);
+		});
+	}
+
+
   buildForm(contact: IContact): FormGroup {
 
 		const group = this.formBuilder.group({
-			
+
+      contactType:        [contact.contactType,         [] ],
+      alias:              [contact.alias,               [] ],
+      homeNumber:         [contact.homePhoneNumber,     [] ], 
+      mobileNumber:       [contact.mobilePhoneNumber,   [] ],
+      workNumber:         [contact.workPhoneNumber,     [] ],
+      email:              [contact.email,               [] ],
+      identifications:    [contact.identifications,     [] ],
+      adresses:           [contact.addresses,           [] ],    
+      profession:         [contact.profession,          [] ],
+      jobPosition:        [contact.jobPosition,         [] ]
+
 		});
 
 		return group;
